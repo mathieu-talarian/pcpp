52e15d868d80dcc75ab826b7d85ef543ff833a26
Mathieu MOULLEC <mmoullec@e2r10p10.42.fr>
2018-10-02 12:11:00 +0200

    woa!! this one was really HARD!

--- d01/ex00/includes/Pony.class.hpp
+++ d01/ex00/includes/Pony.class.hpp
@@ -6,24 +6,28 @@
 /*   By: mmoullec <mmoullec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/02 10:15:40 by mmoullec          #+#    #+#             */
-/*   Updated: 2018/10/02 10:34:05 by mmoullec         ###   ########.fr       */
+/*   Updated: 2018/10/02 12:10:23 by mmoullec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PONY_CLASS_HPP
+#define PONY_CLASS_HPP
 #include <iostream>
 
 class Pony
 {
-  public:
-    Pony(std::string name, std::string stack);
-    ~Pony();
+public:
+  Pony(std::string name, std::string stack);
+  ~Pony();
 
-    void Talk(void);
-    void RunAway(void);
-    void ComeBack(void);
-    void SayMyName(void);
+  void Talk(void);
+  void RunAway(void);
+  void ComeBack(void);
+  void SayMyName(void);
 
-  private:
-    std::string _name;
-    std::string _stack;
+private:
+  std::string _name;
+  std::string _stack;
 };
+
+#endif
