66eb0640383681f50ff426d97c486bf6c35a3674
Mathieu Moullec <mathieu.moullec@gmail.com>
2018-10-04 04:16:26 +0200

    This is where it all begins...

--- d01/ex09/src/main.cpp
+++ d01/ex09/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mmoullec <mmoullec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/03 16:41:31 by mmoullec          #+#    #+#             */
-/*   Updated: 2018/10/03 17:18:29 by mmoullec         ###   ########.fr       */
+/*   Updated: 2018/10/03 22:15:41 by mmoullec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,3 +19,7 @@ int main() {
   l.log("console", "a test on stdout");
   return 0;
 }
+#ifndef ${TM_FILENAME/[\.-]/_/g /(.*)//upcase/}
+#define ${TM_FILENAME/[\.-]/_/g /(.*)//upcase/}
+
+#endif
