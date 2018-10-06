d92ff9dc57c59a46d9c018e60060439395a5bf83
Mathieu MOULLEC <mmoullec@e1r12p21.42.fr>
2018-10-06 18:07:07 +0200

    Makefile improve

--- r00/main.cpp
+++ r00/main.cpp
@@ -1,14 +1,24 @@
-
 #include <ncurses.h>
 
-int main(void) {
-  initscr();             // Initialise la structure WINDOW et autres paramètres
-  printw("Hello World"); // Écrit Hello World à l'endroit où le curseur logique
-                         // est positionné
-  refresh(); // Rafraîchit la fenêtre courante afin de voir le message
-             // apparaître
-  getch();   // On attend que l'utilisateur appui sur une touche pour quitter
-  endwin();  // Restaure les paramètres par défaut du terminal
-
-  return 0;
+int main(int argc, char *argv[])
+{	initscr();			/* Start curses mode 		*/
+	start_color();			/* Start color functionality	*/
+	
+	init_pair(1, COLOR_CYAN, COLOR_BLACK);
+	printw("A Big string which i didn't care to type fully ");
+	mvchgat(0, 0, -1, A_BLINK, 1, NULL);	
+	/* 
+	 * First two parameters specify the position at which to start 
+	 * Third parameter number of characters to update. -1 means till 
+	 * end of line
+	 * Forth parameter is the normal attribute you wanted to give 
+	 * to the charcter
+	 * Fifth is the color index. It is the index given during init_pair()
+	 * use 0 if you didn't want color
+	 * Sixth one is always NULL 
+	 */
+	refresh();
+    	getch();
+	endwin();			/* End curses mode		  */
+	return 0;
 }
