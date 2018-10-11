75a6b8fbd161df48bcf577faf9ee01938101e1be
Mathieu MOULLEC <mmoullec@e1r12p17.42.fr>
2018-10-11 18:12:19 +0200

    This is not the commit message you are looking for

--- d07/ex02/Array.class.hpp
+++ d07/ex02/Array.class.hpp
@@ -6,22 +6,61 @@
 /*   By: mmoullec <mmoullec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/11 01:20:47 by mmoullec          #+#    #+#             */
-/*   Updated: 2018/10/11 01:24:44 by mmoullec         ###   ########.fr       */
+/*   Updated: 2018/10/11 18:04:57 by mmoullec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ARRAY_CLASS_HPP
 #define ARRAY_CLASS_HPP
 
-#define uint unsigned int
+#include <exception>
+#include <iostream>
 
 template <typename T> class Array {
 public:
-  Array<T>();
-  Array(uint n);
-  Array(const Array &);
-  Array &operator=(const Array &);
-  ~Array();
+  Array<T>() : _n(0) {
+    this->_array = NULL;
+    return;
+  }
+
+  Array<T>(uint n) : _n(n) { this->_array = new T[this->_n](); };
+
+  Array<T>(const Array<T> &cpy) {
+    this->_array = new T[cpy.getN()]();
+    this->_n = cpy.getN();
+    for (uint i = 0; i < cpy.getN(); i++)
+      this->_array[i] = cpy[i];
+    *this = cpy;
+  };
+
+  Array<T> &operator=(const Array<T> &cpy) {
+    if (this->_array)
+      delete[] this->_array;
+    this->_array = new T[cpy._n];
+    this->_n = cpy.getN();
+    for (uint i = 0; i < cpy._n; i++)
+      this->_array[i] = cpy[i];
+    return *this;
+  };
+
+  T &operator[](uint n) const {
+    if (n < 0 || n >= this->_n)
+      throw Array::OutOfLimitsException();
+    return this->_array[n];
+  };
+
+  ~Array<T>() { delete[] this->_array; };
+
+  class OutOfLimitsException : public std::exception {
+  public:
+    OutOfLimitsException(){};
+    virtual ~OutOfLimitsException() throw(){};
+
+    virtual const char *what() const throw() { return ("Out of limits"); };
+  };
+
+  uint getN() const { return this->_n; };
+  void setN(uint n) { this->_n = n; };
 
 private:
   uint _n;
---
+++ d07/ex02/main.cpp
@@ -0,0 +1,80 @@
+#include "Array.class.hpp"
+#include <iostream>
+
+int main() {
+  srand(time(NULL));
+  Array<int> *t = new Array<int>(12);
+  try {
+    for (unsigned int i = 0; i < 12; i++)
+      (*t)[i] = i;
+    int i = (*t)[1];
+    std::cout << i << std::endl;
+  } catch (Array<int>::OutOfLimitsException &e) {
+    std::cout << e.what() << std::endl;
+  } catch (std::exception &e) {
+    std::cout << e.what() << std::endl;
+  }
+
+  Array<int> *Trick = new Array<int>(42);
+  try {
+    for (int i = 0; i < 42; i++) {
+      (*Trick)[i] = i;
+      std::cout << (*Trick)[i] << std::endl;
+    }
+    // Trick[i] = 12;
+    // Array<int> *cpy = new Array<int>(*Trick);
+  } catch (std::exception &e) {
+    std::cout << e.what() << std::endl;
+  }
+
+  Array<int> *cpy2 = new Array<int>();
+  cpy2 = Trick;
+
+  ////////////////////////////////////////////////
+  Array<float> v(12);
+  for (float y = 0.0f; y < 12.0f; y++) {
+    (v)[y] = y + 12.56f;
+  }
+  try {
+    std::cout << static_cast<float>((v)[11]) << std::endl;
+  } catch (Array<float>::OutOfLimitsException &e) {
+    std::cout << e.what() << std::endl;
+  } catch (std::exception &e) {
+    std::cout << e.what() << std::endl;
+  }
+
+  ////////////////////////////////////////////////
+  Array<float> vv(v);
+  try {
+    std::cout << static_cast<float>((vv)[11]) << std::endl;
+  } catch (Array<float>::OutOfLimitsException &e) {
+    std::cout << e.what() << std::endl;
+  } catch (std::exception &e) {
+    std::cout << e.what() << std::endl;
+  }
+  Array<float> vvv = vv;
+  try {
+    std::cout << static_cast<float>((vvv)[11]) << std::endl;
+  } catch (Array<float>::OutOfLimitsException &e) {
+    std::cout << e.what() << std::endl;
+  } catch (std::exception &e) {
+    std::cout << e.what() << std::endl;
+  }
+
+  ////////////////////////////////////////////////
+  Array<std::string> strs(1);
+  for (size_t i = 0; i < 1; i++) {
+    strs[i] = std::string("asdlkfjsadf");
+  }
+  try {
+    std::cout << static_cast<std::string>((strs)[0]) << std::endl;
+  } catch (Array<float>::OutOfLimitsException &e) {
+    std::cout << e.what() << std::endl;
+  } catch (std::exception &e) {
+    std::cout << e.what() << std::endl;
+  }
+
+  ////////////////////////////////////////////////
+  delete t;
+  delete Trick;
+}
